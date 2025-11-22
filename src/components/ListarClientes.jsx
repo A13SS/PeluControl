c0cab65de1f8f9ab6effd808433d66e5d618a3ca
Alejandro Fernandez <aferesc1409@gmail.com>
2025-11-22 13:59:43 +0100

    pelucontrol proyecto 2 acabado

--- src/components/ListarClientes.jsx
+++ src/components/ListarClientes.jsx
@@ -1,10 +1,21 @@
-import { clientesIniciales } from '../data/clientes';
 import Busqueda from './Busqueda';
 import MuestraCliente from './MuestraCliente';
 import NoResultados from './NoResultados';
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import '../styles/ListarClientes.css';
 
+const clientesIniciales = [
+  { id: 1, nombre: "Laura González", telefono: "644123123" },
+  { id: 2, nombre: "Carlos Ruiz", telefono: "655321321" },
+  { id: 3, nombre: "Marta Pérez", telefono: "699112233" },
+  { id: 4, nombre: "Ale Fernandez", telefono: "654235678" },
+  { id: 5, nombre: "Juan Pabón", telefono: "698560438" },
+  { id: 6, nombre: "David Jiménez", telefono: "645607943" },
+  { id: 7, nombre: "Gerónimo Molero", telefono: "630485960" },
+  { id: 8, nombre: "Iris Castro", telefono: "670589703" },
+  { id: 9, nombre: "Patricia Chico", telefono: "63045670" },
+  { id: 10, nombre: "Silvia Chico", telefono: "620586970" },
+];
 
 function ListarClientes() {
   const [busqueda, setBusqueda] = useState('');
@@ -12,7 +23,45 @@ function ListarClientes() {
   const [pagina, setPagina] = useState(1);
   const [porPagina, setPorPagina] = useState(3);  //los iniciales antes de añadir nuevos clientes
 
-  const filtrados = clientesIniciales.filter(cliente =>
+  const [clientes, setClientes] = useState([]);      //Reemplazará a clientesIniciales después de cargar
+  const [cargando, setCargando] = useState(true);
+  const [error, setError] = useState(false);
+
+
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      //Si no hay error, se cargan los clientes
+      setClientes(clientesIniciales);
+      setCargando(false);
+    }, 2000);
+
+    //Limpieza del timer si el componente se desmonta
+    return () => clearTimeout(timer);
+  }, []);
+
+  const forzarError = () => {
+    setError(true);
+    setCargando(false);
+  };
+
+  if (cargando) {
+    return (<div className="listarClientesCargando">Cargando clientes...
+      <button
+        className="listarClientesBoton"
+        onClick={forzarError}
+      >
+        Simular error de carga
+      </button>
+    </div>
+    );
+  }
+
+  if (error) {
+    return <div className="listarClientesError">Error al cargar los datos</div>;
+  }
+
+  const filtrados = clientes.filter(cliente =>
     cliente.nombre.toLowerCase().includes(busqueda.toLowerCase()) ||
     cliente.telefono.includes(busqueda)
   )
@@ -72,7 +121,7 @@ function ListarClientes() {
       <div className="listarClientesControles">
         <button className="listarClientesBoton" onClick={() => cambiarOrden('nombre')}>Ordenar por Nombre</button>
         <button className="listarClientesBoton" onClick={() => cambiarOrden('telefono')}>Ordenar por Teléfono</button>
-    
+
         {/*Selector de clientes por página*/}
         <label className="listarClientesEtiqueta">
           Mostrar:
@@ -89,7 +138,7 @@ function ListarClientes() {
 
       <div className="listarClientesLista">
         {clientesMostrados.length > 0 ?
-        (clientesMostrados.map(cliente => <MuestraCliente key={cliente.id} cliente={cliente} />)) : (<NoResultados />)}
+          (clientesMostrados.map(cliente => <MuestraCliente key={cliente.id} cliente={cliente} />)) : (<NoResultados />)}
       </div>
 
 
